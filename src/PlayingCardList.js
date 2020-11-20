36817d6682122c24c2422b78fb61937b28e89d7e
kelly wolosonovich <k.wolosonovich@gmail.com>
2020-11-20 16:03:20 -0700

    implemented localStorage and useAxios

--- src/PlayingCardList.js
+++ src/PlayingCardList.js
@@ -3,17 +3,23 @@ import uuid from "uuid";
 import axios from "axios";
 import PlayingCard from "./PlayingCard";
 import "./PlayingCardList.css";
+import useAxios from "./hooks/useAxios"
 
 /* Renders a list of playing cards.
  * Can also add a new card at random. */
 function CardTable() {
-  const [cards, setCards] = useState([]);
-  const addCard = async () => {
-    const response = await axios.get(
-      "https://deckofcardsapi.com/api/deck/new/draw/"
-    );
-    setCards(cards => [...cards, { ...response.data, id: uuid() }]);
-  };
+  const [cards, addCard] = useAxios(
+    "cards",
+    "https://deckofcardsapi.com/api/deck/new/draw/"
+  );
+
+  // const addCard = async () => {
+  //   const response = await axios.get(
+  //     "https://deckofcardsapi.com/api/deck/new/draw/"
+  //   );
+  //   setCards(cards => [...cards, { ...response.data, id: uuid() }]);
+  // };
+
   return (
     <div className="PlayingCardList">
       <h3>Pick a card, any card!</h3>
@@ -21,8 +27,8 @@ function CardTable() {
         <button onClick={addCard}>Add a playing card!</button>
       </div>
       <div className="PlayingCardList-card-area">
-        {cards.map(cardData => (
-          <PlayingCard key={cardData.id} front={cardData.cards[0].image} />
+        {cards.map(card => (
+          <PlayingCard key={card.id} front={card.cards[0].image} />
         ))}
       </div>
     </div>
--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -1,8 +1,44 @@
-import React from "react";
-import Axios from "axios"
+import {useState, useEffect} from "react";
+import axios from "axios";
 
-const useAxios = () => {
 
+const useAxios = (item, url) => {
+  // get responses (card list) from local storage 
+  const [response, setResponse] = useLocalStorage(item)
+
+//   const addItem = async () => {
+//     const responseAPI = await axios.get(url);
+//     setResponse(...response, responseAPI.data);
+//   };
+//   return [response, addItem ];
+// }
+
+  const addItem = async (formatter = (data) => data) => {
+    const responseAPI = await axios.get(url);
+    setResponse((data) => [...data, formatter(responseAPI.data)]);
+  };
 }
 
-export default useAxios;
+const useLocalStorage = (item, initialValue = []) => {
+    // get current list of cards
+    if (localStorage.getItem(item)) {
+        initialValue = JSON.parse(localStorage.getItem(item))
+    }
+     // setting your initialValue to whatever is in the localstorage, and parsing it as json
+    const [value, setValue] = useState(initialValue);
+
+    // every time a new key is added, useEffect will be save card to local storage
+    useEffect(() => {
+        localStorage.setItem(item, JSON.stringify(value));
+    }, [value, item]);
+
+  // return value to useAxios - array of cards
+  return [value, setValue];
+}
+
+
+// export default useLocalStorage;
+
+// export {useAxios, useLocalStorage}
+
+export default useAxios; 
