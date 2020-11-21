9f207eec35743e5fe811ba7c2b2c0e9215af396b
kelly wolosonovich <k.wolosonovich@gmail.com>
2020-11-20 17:52:07 -0700

    troubleshooting useAxios

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -1,44 +1,39 @@
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import uuid from "uuid";
 
-
-
 const useAxios = (item, url) => {
-  // get responses (card list) from local storage 
+  // get responses (card list) from local storage
   const [responses, setResponses] = useLocalStorage(item);
 
-//   const addItem = async () => {
-//     const responseAPI = await axios.get(url);
-//     setResponse(...response, responseAPI.data);
-//   };
-//   return [response, addItem ];
-    const id = uuid() 
 
-  const addResponseData = async (formatter = data => data, restOfUrl = "", id) => {
+  const addResponseData = async (
+    formatter = (data) => data,
+    restOfUrl = "",
+    id = uuid()
+  ) => {
     const response = await axios.get(`${url}${restOfUrl}`);
     setResponses((data) => [...data, formatter(response.data)]);
   };
 
   return [responses, addResponseData];
-}
+};
 
 const useLocalStorage = (item, initialValue = []) => {
-    // get current list of cards
-    if (localStorage.getItem(item)) {
-        initialValue = JSON.parse(localStorage.getItem(item))
-    }
-     // setting your initialValue to whatever is in the localstorage, and parsing it as json
-    const [value, setValue] = useState(initialValue);
+  // get current list of cards
+  if (localStorage.getItem(item)) {
+    initialValue = JSON.parse(localStorage.getItem(item));
+  }
+  // setting your initialValue to whatever is in the localstorage, and parsing it as json
+  const [value, setValue] = useState(initialValue);
 
-    // every time a new key is added, useEffect will be save card to local storage
-    useEffect(() => {
-        localStorage.setItem(item, JSON.stringify(value));
-    }, [value, item]);
+  // every time a new key is added, useEffect will be save card to local storage
+  useEffect(() => {
+    localStorage.setItem(item, JSON.stringify(value));
+  }, [value, item]);
 
   // return value to useAxios - array of cards
   return [value, setValue];
-}
+};
 
-
-export default useAxios; 
+export default useAxios;
